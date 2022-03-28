446d0b9da179f4845497674803e7fdac714cdae7
LeeHaeRang <doco7575@gmail.com>
2022-03-28 20:07:24 +0900

    게임 끝나고 점수 추가 기능

--- src/view/GameEndPage.java
+++ src/view/GameEndPage.java
@@ -4,20 +4,22 @@ import controller.PageController;
 import model.JsonScore;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
 public class GameEndPage extends JFrame {
-    private JPanel MainPanel;
-    private JPanel InsetPanel;
+
+    private JPanel InsertPanel;
     private JPanel ButtonPanel;
     private JButton nameEnterButton;
     private JTextField nameTextField;
     private JButton mainButton;
     private JButton exitButton;
     private JButton gameRestartButton;
-    private JLabel gameScore;
+    private JLabel gameScoreLabel;
+    private JLabel nameLabel;
     private JPanel scoreBoardPanel;
     private PageController pageController;
 
@@ -31,9 +33,46 @@ public class GameEndPage extends JFrame {
 
     private void initialize() {
 
-        scoreBoardPanel=new ScorePage().returnScorePanel();
-        this.add(scoreBoardPanel);
-        this.add(MainPanel);
+        //프레임 설정
+        BorderLayout bl = new BorderLayout();
+        this.setLayout(bl);
+        
+        //패널 생성
+        scoreBoardPanel=new JPanel();
+        InsertPanel = new JPanel();
+        ButtonPanel = new JPanel();
+
+        GridLayout gl = new GridLayout(3,1);
+        ButtonPanel.setLayout(gl);
+
+        //컴포넌트 생성
+        nameEnterButton=new JButton("Enter");
+        nameTextField = new JTextField();
+        gameScoreLabel = new JLabel("1111");
+        nameLabel=new JLabel("Name");
+
+        mainButton=new JButton("Main");
+        gameRestartButton = new JButton("Game Restart");
+        exitButton = new JButton("Exit");
+
+
+        //패널 컴포넌트 연결
+        scoreBoardPanel=new scoreBoard();
+
+        InsertPanel.add(nameLabel);
+        InsertPanel.add(nameTextField);
+        InsertPanel.add(gameScoreLabel);
+        InsertPanel.add(nameEnterButton);
+
+        ButtonPanel.add(mainButton);
+        ButtonPanel.add(gameRestartButton);
+        ButtonPanel.add(exitButton);
+
+
+        //패널 add
+        this.add(scoreBoardPanel,BorderLayout.CENTER);
+        this.add(InsertPanel,BorderLayout.SOUTH);
+        this.add(ButtonPanel,BorderLayout.EAST);
 
         this.setSize(500, 500);
         this.setVisible(true);
@@ -86,7 +125,26 @@ public class GameEndPage extends JFrame {
                     JsonScore score=new JsonScore();
                     score.save(nameTextField.getText(),1111);
 
-                    
+                    updateScore();
+
+                    //변경된 부분 하이라이트 추가
+                    for(int i=0;i<10;i++)
+                    {
+                        //인덱스로 못 찾는 거 같은데
+                        Component c = scoreBoardPanel.getComponent(3*i+2);
+                        if(c instanceof JLabel)
+                        {
+                            JLabel jl=(JLabel) c;
+                            if(jl.getText().equals(nameTextField.getText()))
+                            {
+                                System.out.println(jl.getText());
+                                jl.setOpaque(true);
+                                jl.setForeground(Color.RED);
+                            }
+                        }
+                    }
+
+
 
                 }
                 else System.out.println("Name is empty");
@@ -94,5 +152,14 @@ public class GameEndPage extends JFrame {
         });
 
     }
+
+    private void updateScore()
+    {
+        this.remove(scoreBoardPanel);
+        this.scoreBoardPanel=new scoreBoard();
+        this.add(scoreBoardPanel,BorderLayout.CENTER);
+        this.validate();
+    }
+
 }
 
--- src/view/ScorePage.java
+++ src/view/ScorePage.java
@@ -5,6 +5,7 @@ import controller.HashMapParser;
 import controller.PageController;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -20,19 +21,9 @@ public class ScorePage extends JFrame {
     private JPanel ButtonPanel;
     private JButton mainButton;
     private JPanel ScorePanel;
-    private JLabel score1;
-    private JLabel score2;
-    private JLabel score3;
-    private JLabel score4;
-    private JLabel score5;
-    private JLabel score6;
-    private JLabel score7;
-    private JLabel score8;
-    private JLabel score9;
-    private JLabel score10;
+    private JLabel titleLabel;
 
-    private JLabel[] scoreLables = new JLabel[]{
-        score1, score2, score3, score4, score5, score6, score7, score8, score9, score10};
+
     private PageController pageController;
 
     public ScorePage() {
@@ -44,14 +35,39 @@ public class ScorePage extends JFrame {
     }
 
     private void initialize() {
-        this.add(MainPanel);
+        
+        //프레임 설정
+        BorderLayout bl = new BorderLayout();
+        this.setLayout(bl);
+
+        //패널 생성
+        ScorePanel=new JPanel();
+        ButtonPanel= new JPanel();
+        MainPanel= new JPanel();
+
+        //컴포넌트 생성
+        mainButton=new JButton("Main");
+        titleLabel=new JLabel("Score");
+
+        //패널 컴포넌트 연결
+        ScorePanel.add(new scoreBoard());
+        ButtonPanel.add(mainButton);
+
+        //패널 add
+        this.add(titleLabel,BorderLayout.NORTH);
+        this.add(ScorePanel,BorderLayout.CENTER);
+        this.add(ButtonPanel,BorderLayout.SOUTH);
+
+
+
+
         this.setSize(500, 500);
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// x표 눌었을 때 프로세스 종료되게 만듦.
 
         this.setLocationRelativeTo(null);//화면 가운데에 생성
 
-        setScoreBoard();
+
 
         //포커스를 이 화면에 맟춰서 키 이벤트 받게 만듦
         requestFocus();
@@ -74,27 +90,7 @@ public class ScorePage extends JFrame {
         return this.ScorePanel;
     }
 
-    private void setScoreBoard() {
-        HashMap<String, Integer> scoreInfo = new JsonScore().getList();
-        HashMapParser hashmapparser = new HashMapParser();
 
-        //hashmap은 정렬이 안 되어서 list로 변경
-        java.util.List<Map.Entry<String,Integer>> orederedScoreInfo=hashmapparser.orederByDescent(scoreInfo);
-
-
-
-
-
-
-
-        System.out.println(orederedScoreInfo + "  printed at ScorePage.Java");
-        for (int i = 0; i < 10; i++) {
-
-            scoreLables[i].setText(Integer.toString(orederedScoreInfo.get(i).getValue()) + " by "+orederedScoreInfo.get(i).getKey());
-
-        }
-
-    }
 
 
 }
