3e04f4d4a13d9675d84168507c47857b01edda52
LeeHaeRang <doco7575@gmail.com>
2022-05-12 02:54:58 +0900

    대전모드 화면 form 작성

---
+++ src/view/VersusGamePage.java
@@ -0,0 +1,33 @@
+package view;
+
+import javax.swing.*;
+
+public class VersusGamePage {
+    private JPanel mainPanel;
+    private JPanel palyer1Panel;
+    private JPanel gameBoardPanel1;
+    private JPanel nextBlockPanel1;
+    private JPanel scorePanel1;
+    private JPanel passedBlockPanel1;
+    private JTextPane gameBoardPane1;
+    private JTextPane nextBlockPane1;
+    private JTextPane passedBlockPane1;
+    private JLabel scoreLabel1;
+    private JPanel player2Panel;
+    private JPanel gameBoardPanel2;
+    private JTextPane gameBoardPane2;
+    private JPanel nextBlockPanel2;
+    private JTextPane nextBlockPane2;
+    private JPanel passedBlockPanel2;
+    private JTextPane passedBlockPane2;
+    private JPanel scorePanel2;
+    private JLabel scoreLabel2;
+    private JPanel buttonPanel;
+    private JButton stopButton;
+    private JButton mainButton;
+    private JButton exitButton;
+
+    private void createUIComponents() {
+
+    }
+}
