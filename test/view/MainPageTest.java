f1d6489ad8769517c831bb0a881da03d27c03ed7
DESKTOP-K84G98C\82103 <enjxz97382@gmail.com>
2022-05-19 14:06:06 +0900

    MainPageTest코드 완료

--- test/view/MainPageTest.java
+++ test/view/MainPageTest.java
@@ -3,8 +3,12 @@ package view;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
+import java.awt.event.KeyEvent;
 import java.lang.reflect.Field;
 
+import static java.awt.event.KeyEvent.VK_DOWN;
+import static java.awt.event.KeyEvent.VK_ENTER;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainPageTest {
@@ -12,10 +16,12 @@ class MainPageTest {
     @Test
     @DisplayName("키 입력 테스트")
     void keyPressedTest() throws NoSuchFieldException {
-        MainPage mainPage = new MainPage();
-        Field temp = MainPage.class.getDeclaredField("setKeyEventController");
-        temp.setAccessible(true);
-        temp.keyP
+        MainPage.MainPageKeyListener keyListener = new MainPage().new MainPageKeyListener();
+        JButton jButtons = new JButton();
+        KeyEvent keyEvent = new KeyEvent(jButtons, 0, 0, 1, VK_DOWN);
+        keyListener.keyPressed(keyEvent);
+        keyEvent.setKeyCode(VK_ENTER);
+        keyListener.keyPressed(keyEvent);
     }
 
 }
